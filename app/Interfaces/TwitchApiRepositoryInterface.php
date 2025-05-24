45f9cc0f6600b557bc45c2b25b3c83ee52bd1063
IsraelHuro <israelhuro95@gmail.com>
2025-05-24 21:18:13 +0200

    [#43] - añado al interfaz el getStreamerById

--- app/Interfaces/TwitchApiRepositoryInterface.php
+++ app/Interfaces/TwitchApiRepositoryInterface.php
@@ -5,4 +5,12 @@ namespace App\Interfaces;
 interface TwitchApiRepositoryInterface
 {
     public function getStreams(): array;
+
+    /**
+     * Obtiene la información cruda de un streamer por su ID.
+     *
+     * @param  string  $id  ID numérico del streamer
+     * @return array        Datos crudos de Twitch (o array vacío si no se encuentra).
+     */
+    public function getStreamerById(string $id): array;
 }
